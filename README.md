eeadd034bff6254eaf15895920c2c2a43fed4e2b
Zhen Yi <zhenyi613@gmail.com>
2025-06-02 17:19:48 +0800

    feat: Probability Trial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Egghub
+# Egghub
+
+A collection of random tools and utilities that I've built to help improve my everyday life. This repository serves as a hub for various personal projects and scripts that solve common problems or automate routine tasks.
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Homepage from "./pages/Homepage";
+import Probability from "./pages/Probability";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,7 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/probability" element={<Probability />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
